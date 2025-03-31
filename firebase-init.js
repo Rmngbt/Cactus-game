efcc7f476bc98ec8598f727b8dff82ea993b8396
Rmngbt <74736494+Rmngbt@users.noreply.github.com>
2025-03-31 22:58:03 +0200

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,46 +1,20 @@
-// firebase-sync.js
-import { db, ref, set, onValue } from "./firebase-init.js";
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 
-let currentRoomId = null;
-let currentUsername = null;
-
-function setSessionData(roomId, username) {
-  currentRoomId = roomId;
-  currentUsername = username;
-}
-
-function syncTurnToFirebase(turn) {
-  if (!currentRoomId) return;
-  set(ref(db, `games/${currentRoomId}/currentPlayer`), turn);
-}
-
-function watchTurnChanges(callback) {
-  if (!currentRoomId) return;
-  const turnRef = ref(db, `games/${currentRoomId}/currentPlayer`);
-  onValue(turnRef, (snapshot) => {
-    const val = snapshot.val();
-    if (val !== null) callback(val);
-  });
-}
-
-function syncGameState(key, value) {
-  if (!currentRoomId) return;
-  set(ref(db, `games/${currentRoomId}/${key}`), value);
-}
-
-function watchGameState(key, callback) {
-  if (!currentRoomId) return;
-  const stateRef = ref(db, `games/${currentRoomId}/${key}`);
-  onValue(stateRef, (snap) => {
-    const state = snap.val();
-    callback(state);
-  });
-}
-
-export {
-  setSessionData,
-  syncTurnToFirebase,
-  watchTurnChanges,
-  syncGameState,
-  watchGameState
+const firebaseConfig = {
+  apiKey: "AIzaSyBd2O4MWVNlY5MOVffdcvMrkj2lLxJcdv0",
+  authDomain: "cactus-game-12ae9.firebaseapp.com",
+  projectId: "cactus-game-12ae9",
+  storageBucket: "cactus-game-12ae9.appspot.com",
+  messagingSenderId: "852427558969",
+  appId: "1:852427558969:web:0b292c74c6305dc348fde8",
+  databaseURL: "https://cactus-game-12ae9-default-rtdb.firebaseio.com/"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+export { app, db, auth, ref, set, onValue };
